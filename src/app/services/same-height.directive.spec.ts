afaf09900ff293ca7f30eb9641d2164d0caa410b
Jan Moxter <jan.moxter@innobix.com>
2018-04-18 01:16:36 -0500

    - adding same size directive

---
+++ src/app/services/same-height.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SameHeightDirective } from './same-height.directive';
+
+describe('SameHeightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SameHeightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
