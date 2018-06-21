35ae13c88240604bfe7f070c9e3324e5c685ed8b
Jan Moxter <jan.moxter@innobix.com>
2018-06-21 14:28:04 -0500

    App Component Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,54 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  MissingTranslationHandler,
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+  TranslateParser,
+  TranslateService,
+  TranslateStore,
+  USE_DEFAULT_LANG
+} from '@ngx-translate/core';
+
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import { HttpLoaderFactory } from './app.module';
 import { MaterialModule } from './material/material.module';
+import {
+  AdminService,
+  AuthService,
+  ConfigService,
+  WorkspaceService
+} from './services/index';
+import { SharedModule } from './shared/shared.module';
+import { ThemePickerComponent } from './theme-picker/theme-picker.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         MaterialModule,
-        SharedModule
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        SharedModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [
+        AuthService,
+        ConfigService,
+        WorkspaceService
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ThemePickerComponent
       ],
     }).compileComponents();
   }));
@@ -19,16 +56,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
