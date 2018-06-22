84f887df68d889c0bb5603a96c98f579299769a0
Jan Moxter <jan.moxter@innobix.com>
2018-06-21 22:53:39 -0500

    Rework of Card Component to only use Reactive forms

--- src/app/payment/card/card.component.ts
+++ src/app/payment/card/card.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 import { Payment, PaymentType } from '../../model/index';
 import { PaymentService, UtilsService } from '../../services/index';
@@ -10,17 +10,24 @@ import { PaymentForm } from '../payment-form';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent extends PaymentForm {
+export class CardComponent extends PaymentForm implements OnInit {
   PaymentType = PaymentType;
-  paymentTypes = [ PaymentType.DEBITCARD, PaymentType.CREDITCARD ];
-  paymentType: PaymentType = PaymentType.DEBITCARD;
+  paymentTypes = [PaymentType.DEBITCARD, PaymentType.CREDITCARD];
 
   constructor(paymentService: PaymentService, utilsService: UtilsService,
     fb: FormBuilder) {
     super(paymentService, utilsService, fb);
   }
 
+  ngOnInit() {
+    this.paymentForm = this.fb.group({
+      'amount': ['0.00', Validators.min(0)],
+      'paymentType': [PaymentType.DEBITCARD]
+    });
+    this.paymentService.currentPayments.subscribe(_payments => this.payments = _payments);
+  }
+
   getPaymentType(): PaymentType {
-    return this.paymentType;
+    return this.paymentForm.value.paymentType;
   }
 }
