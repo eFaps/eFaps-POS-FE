2d14fa4abb21f2b1fb0e1bf6c5157d5d7112135b
Jan Moxter <jan.moxter@innobix.com>
2018-08-09 01:12:11 -0500

    Update unit tests

--- src/app/balance/balance-payment-list/balance-payment-list.component.spec.ts
+++ src/app/balance/balance-payment-list/balance-payment-list.component.spec.ts
@@ -1,6 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockPipe } from 'ng-mocks';
+import { Observable } from 'rxjs';
 
+import { MaterialModule } from '../../material/material.module';
+import { BalanceService, DocumentService } from '../../services';
 import { BalancePaymentListComponent } from './balance-payment-list.component';
+
+class BalanceServiceStub {
+  currentBalance = new Observable(observer => {
+    observer.next([]);
+  });
+}
+
+class DocumentServiceStub { }
 
 describe('BalancePaymentListComponent', () => {
   let component: BalancePaymentListComponent;
@@ -8,7 +22,18 @@ describe('BalancePaymentListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BalancePaymentListComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        MaterialModule
+      ],
+      providers: [
+        { provide: DocumentService, useClass: DocumentServiceStub },
+        { provide: BalanceService, useClass: BalanceServiceStub }
+      ],
+      declarations: [
+        BalancePaymentListComponent,
+        MockPipe(TranslatePipe)
+      ]
     })
     .compileComponents();
   }));
