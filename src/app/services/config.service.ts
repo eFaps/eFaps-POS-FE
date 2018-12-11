57a9b3bd6e60615eb76365bf6cb3016845fea9ca
Jan Moxter <jan.moxter@innobix.com>
2018-12-10 21:04:36 -0500

    Config from localstorage for electron

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { LocalStorageService } from 'ngx-store';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ConfigService {
   public baseUrl: string;
   public socketUrl: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService) { }
 
   load(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.http.get('assets/config.json')
-        .subscribe((envResponse: any) => {
-          this.baseUrl = envResponse.baseUrl;
-          this.socketUrl = envResponse.socketUrl;
-          resolve(true);
-        });
+    return new Promise((resolve) => {
+      if (environment.electron) {
+        const baseUrl = this.localStorageService.get("baseUrl");
+        if (baseUrl) {
+          this.baseUrl = baseUrl;
+        } else {
+          console.log('No baseUrl')
+        }
+        const socketUrl = this.localStorageService.get("socketUrl");
+        if (socketUrl) {
+          this.socketUrl = socketUrl;
+        } else {
+          console.log('No socketUrl')
+        }
+        resolve(true);
+      } else {
+        this.http.get('assets/config.json')
+          .subscribe((envResponse: any) => {
+            this.baseUrl = envResponse.baseUrl;
+            this.socketUrl = envResponse.socketUrl;
+            resolve(true);
+          });
+      }
     });
   }
 }
