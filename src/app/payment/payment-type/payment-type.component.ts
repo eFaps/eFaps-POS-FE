80450e8e340b0399d66695fd5a504a9288d72649
Jan Moxter <jan.moxter@moxter.net>
2019-07-03 00:24:09 -0500

    Payment

--- src/app/payment/payment-type/payment-type.component.ts
+++ src/app/payment/payment-type/payment-type.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-payment-type',
@@ -12,4 +13,7 @@ export class PaymentTypeComponent implements OnInit {
   ngOnInit() {
   }
 
+  get auto() {
+    return environment.electron;
+  }
 }
