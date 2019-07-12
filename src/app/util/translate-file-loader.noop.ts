a4e841cb11f324e0ef20796ccffa94f0af8ec6f0
Jan Moxter <jan.moxter@moxter.net>
2019-07-12 00:31:25 -0500

    Update observables use

--- src/app/util/translate-file-loader.noop.ts
+++ src/app/util/translate-file-loader.noop.ts
@@ -2,9 +2,9 @@ import { TranslateLoader } from '@ngx-translate/core';
 import { Observable } from 'rxjs/Observable';
 
 export class TranslateFileLoader implements TranslateLoader {
-  constructor(private prefix: string = 'assets/i18n', private suffix: string = '.json') { }
+  constructor(private _prefix: string = 'assets/i18n', private _suffix: string = '.json') { }
 
-  public getTranslation(lang: string): Observable<any> {
-    return Observable.of();
+  public getTranslation(_lang: string): Observable<any> {
+    return new Observable();
   }
 }
