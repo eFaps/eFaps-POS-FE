9b138d8e3595cbf6de36c34250076f704d60d49e
Jan Moxter <jan.moxter@moxter.net>
2019-08-08 00:25:38 -0500

    Adding possiblity to print balance summary

--- src/app/balance/balance-summary/balance-summary.component.ts
+++ src/app/balance/balance-summary/balance-summary.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { BalanceService } from '../../services';
-import { Balance, BalanceSummary } from '../../model';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { Subscription } from 'rxjs';
+
+import { Balance, BalanceSummary } from '../../model';
+import { BalanceService, PrintService, WorkspaceService } from '../../services';
+import { PrintDialogComponent } from '../../shared/print-dialog/print-dialog.component';
 
 @Component({
   selector: 'app-balance-summary',
@@ -9,24 +12,47 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./balance-summary.component.scss']
 })
 export class BalanceSummaryComponent implements OnInit {
-
   _balance: Balance;
-  subscribtion$ = new Subscription()
+  subscription$ = new Subscription()
   summary: BalanceSummary;
+  printer: boolean;
+  private workspaceOid: string;
 
-  constructor(private balanceService: BalanceService) { }
+  constructor(private balanceService: BalanceService,
+    private workspaceService: WorkspaceService,
+    private printService: PrintService,
+    private dialog: MatDialog) { }
 
   ngOnInit() {
-
+    this.subscription$.add(this.workspaceService.currentWorkspace.subscribe({
+      next: workspace => {
+        if (workspace) {
+          this.printer = workspace.printCmds.some(x => x.target === 'BALANCE');
+          this.workspaceOid = workspace.oid;
+        } else {
+          this.printer = false;
+        }
+      }
+    }))
   }
 
   @Input()
   set balance(balance: Balance) {
     this._balance = balance;
     if (balance) {
-      this.subscribtion$.add(this.balanceService.getSummary(balance).subscribe({
+      this.subscription$.add(this.balanceService.getSummary(balance).subscribe({
         next: summary => this.summary = summary
       }));
     }
+  }
+
+  print() {
+    this.dialog.open(PrintDialogComponent, {
+      data: this.printService.printBalance(this.workspaceOid, this._balance.id)
+    });
+  }
+
+  hasBalance(): boolean {
+    return this._balance != null;
   }
 }
