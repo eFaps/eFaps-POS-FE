6bf11ac2d98c5df57946807f214016547d291a6d
jan-moxter <jan.moxter@moxter.net>
2019-09-07 13:59:46 -0500

    Refresh Token implementation

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,10 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as jwtDecode from 'jwt-decode';
 import { LocalStorage } from 'ngx-store';
-import { BehaviorSubject ,  Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Roles } from '../model/index';
+import { Tokens } from '../model/tokens';
 import { ConfigService } from './config.service';
 
 @Injectable()
@@ -15,40 +16,66 @@ export class AuthService {
   private eventSource = new BehaviorSubject<string>('');
   currentEvent = this.eventSource.asObservable();
 
+  private refreshing = false;
+
   constructor(private http: HttpClient, private config: ConfigService) {
   }
 
   login(username: string, password: string): Observable<boolean> {
-    const href = this.config.baseUrl + '/authenticate';
-    return this.http.post(href, { userName: username, password: password })
-      .pipe(map((response: any) => {
-        const token = response.token;
-        if (token) {
-          this.currentUser = { username: username, token: token };
+    const href = `${this.config.baseUrl}/authenticate`;
+    return this.http.post<Tokens>(href, { userName: username, password: password })
+      .pipe(map((response) => {
+        if (response.accessToken) {
+          this.currentUser = {
+            username: username,
+            tokens: response
+          };
           this.currentUser.save();
           this.eventSource.next('login');
+          this.refreshing = false;
           return true;
         } else {
           return false;
         }
       }));
-
   }
+
+  private refreshToken() {
+    this.refreshing = true;
+    const href = `${this.config.baseUrl}/refreshauth`;
+    this.http.post<Tokens>(href, { refreshToken: this.getRefreshToken() }).subscribe({
+      next: response => {
+        if (response.accessToken) {
+          this.currentUser.tokens = response;
+          this.currentUser.save();
+          this.eventSource.next('refresh');
+        } else {
+          this.logout();
+        }
+        this.refreshing = false;
+      }
+    });
+  }
+
 
   logout(): void {
     this.currentUser = null;
     this.eventSource.next('logout');
   }
 
-  getToken(): string {
-    return this.currentUser && this.currentUser.token;
+  getAccessToken(): string {
+    return this.currentUser && this.currentUser.tokens.accessToken;
+  }
+
+  private getRefreshToken(): string {
+    return this.currentUser && this.currentUser.tokens.refreshToken;
   }
 
   getCurrentUsername(): string {
     return this.currentUser && this.currentUser.username;
   }
 
-  getTokenExpirationDate(_token: string): Date {
+  private getTokenExpirationDate(_token: string): Date {
     const decoded = jwtDecode(_token);
 
     if (decoded.exp === undefined) { return null; }
@@ -59,19 +86,23 @@ export class AuthService {
   }
 
   isTokenExpired(_token?: string): boolean {
-    if (!_token) { _token = this.getToken(); }
+    if (!_token) { _token = this.getAccessToken(); }
     if (!_token) { return true; }
 
     const date = this.getTokenExpirationDate(_token);
     if (date === undefined) { return false; }
+
+    if (!this.refreshing && (date.valueOf() - new Date().valueOf()) < (2 * 60 * 1000)) {
+      this.refreshToken();
+    }
     return !(date.valueOf() > new Date().valueOf());
   }
 
   hasRole(_role: Roles) {
     if (this.isTokenExpired()) {
-        return false;
+      return false;
     }
-    const decoded = jwtDecode(this.getToken());
+    const decoded = jwtDecode(this.getAccessToken());
     const roles: string[] = decoded.roles;
     return roles.find(x => x === Roles[_role]) ? true : false;
   }
--- src/app/services/jwt.interceptor.ts
+++ src/app/services/jwt.interceptor.ts
@@ -10,10 +10,10 @@ export class JwtInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const currentUser = this.authService.currentUser;
-    if (currentUser && currentUser.token) {
+    if (currentUser && currentUser.tokens) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
+          Authorization: `Bearer ${currentUser.tokens.accessToken}`
         }
       });
     }
