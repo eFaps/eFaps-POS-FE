4568cbee0dc84055bcbf4162a944be62f64a6360
Jan Moxter <jan.moxter@moxter.net>
2019-09-27 00:00:25 -0500

    Adding manual discounts

--- src/app/payment/discount/discount.component.ts
+++ src/app/payment/discount/discount.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { Subscription } from 'rxjs';
-import { WorkspaceService, PaymentService, DiscountService } from '../../services';
+
 import { Discount, DiscountType, Order } from '../../model';
+import { DiscountService, PaymentService, WorkspaceService, UtilsService } from '../../services';
 
 @Component({
   selector: 'app-discount',
@@ -12,29 +14,73 @@ import { Discount, DiscountType, Order } from '../../model';
 export class DiscountComponent implements OnInit, OnDestroy {
   private document: Order;
   private subscriptions$ = new Subscription();
+  amountForm: FormGroup;
+  percentForm: FormGroup;
   _discounts: Discount[] = [];
+  currency: string;
 
   constructor(public dialogRef: MatDialogRef<DiscountComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any, private workspaceService: WorkspaceService,
-    public paymentService: PaymentService, private discountService: DiscountService) { }
+    public paymentService: PaymentService,
+    private discountService: DiscountService,
+    private utilsService: UtilsService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.amountForm = this.fb.group({
+      'amount': ['0.00', [Validators.min(0), Validators.required]],
+    })
+    this.percentForm = this.fb.group({
+      'percent': ['0', [Validators.min(0), Validators.required]],
+    })
+
     this.subscriptions$.add(this.workspaceService.currentWorkspace.subscribe(ws => {
       this._discounts = ws.discounts
     }));
     this.document = this.data;
+    this.currency = this.utilsService.getCurrencySymbol(this.paymentService.currency);
   }
 
-  get percentDiscount() {
+  get percentDiscounts() {
     return this._discounts
-      .filter(discount => discount.type == DiscountType.PERCENT)
+      .filter(discount => discount.type == DiscountType.PERCENT
+        && discount.value && discount.value > 0)
       .sort((d1, d2) => d1.value - d2.value);
   }
 
-  get amountDiscount() {
+  get amountDiscounts() {
     return this._discounts
-      .filter(discount => discount.type == DiscountType.AMOUNT)
+      .filter(discount => discount.type == DiscountType.AMOUNT
+        && discount.value && discount.value > 0)
       .sort((d1, d2) => d1.value - d2.value);
+  }
+
+  get manualPercent(): boolean {
+    return this._discounts.some(discount => discount.type == DiscountType.PERCENT
+      && (!discount.value || discount.value <= 0));
+  }
+
+  get manualAmount(): boolean {
+    return this._discounts.some(discount => discount.type == DiscountType.AMOUNT
+      && (!discount.value || discount.value <= 0));
+  }
+
+  applyPercentAmount() {
+    if (this.percentForm.valid) {
+      const percent = Number(this.percentForm.get("percent").value);
+      const discount = this._discounts.find(discount => discount.type == DiscountType.PERCENT
+        && (!discount.value || discount.value <= 0));
+      this.applyDiscount({ ...discount, value: percent });
+    }
+  }
+
+  applyManualAmount() {
+    if (this.amountForm.valid) {
+      const amount = Number(this.amountForm.get("amount").value);
+      const discount = this._discounts.find(discount => discount.type == DiscountType.AMOUNT
+        && (!discount.value || discount.value <= 0));
+      this.applyDiscount({ ...discount, value: amount });
+    }
   }
 
   applyDiscount(discount: Discount) {
@@ -46,5 +92,4 @@ export class DiscountComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscriptions$.unsubscribe();
   }
-
 }
