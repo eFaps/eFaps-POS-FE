85d7ce4e74db1325704199db2973e4cdf8a1dc38
Jan Moxter <jan.moxter@moxter.net>
2020-01-30 16:59:50 -0500

    Update Test

--- src/app/orders/reassign-dialog/reassign-dialog.component.spec.ts
+++ src/app/orders/reassign-dialog/reassign-dialog.component.spec.ts
@@ -1,5 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { PosConfigToken } from '@efaps/pos-library';
+import { MockComponent } from 'ng-mocks';
 
+import { MaterialModule } from '../../material/material.module';
+import { ReassignItemComponent } from '../reassign-item/reassign-item.component';
 import { ReassignDialogComponent } from './reassign-dialog.component';
 
 describe('ReassignDialogComponent', () => {
@@ -8,9 +14,21 @@ describe('ReassignDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReassignDialogComponent ]
+      imports: [
+        MaterialModule,
+        HttpClientModule,
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: PosConfigToken, useValue: {} },
+      ],
+      declarations: [
+        MockComponent(ReassignItemComponent),
+        ReassignDialogComponent
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
