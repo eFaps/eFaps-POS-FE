920c044e86882d644035841cb7f98c923c342c2f
Jan Moxter <jan.moxter@moxter.net>
2020-01-30 17:25:42 -0500

    Update Test

--- src/app/payment/payment-type/payment-type.component.spec.ts
+++ src/app/payment/payment-type/payment-type.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaymentTypeComponent } from './payment-type.component';
+import { MaterialModule } from '../../material/material.module';
+import { MockComponent } from 'ng-mocks';
+import { CashComponent } from '../cash/cash.component';
+import { CardComponent } from '../card/card.component';
+import { DiscountComponent } from '../discount/discount.component';
+import { FreeComponent } from '../free/free.component';
+import { AutoComponent } from '../auto/auto.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PosConfigToken } from '@efaps/pos-library';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('PaymentTypeComponent', () => {
   let component: PaymentTypeComponent;
@@ -8,7 +18,22 @@ describe('PaymentTypeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PaymentTypeComponent ]
+      imports: [
+        MaterialModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        {provide:PosConfigToken, useValue: {}}
+      ],
+      declarations: [
+        MockComponent(AutoComponent),
+        MockComponent(CashComponent),
+        MockComponent(CardComponent),
+        MockComponent(DiscountComponent),
+        MockComponent(FreeComponent),
+        PaymentTypeComponent
+       ]
     })
     .compileComponents();
   }));
