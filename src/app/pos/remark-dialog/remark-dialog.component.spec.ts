5f1b11ef346d846ef60bd80c920e99291945c3ba
Jan Moxter <jan.moxter@moxter.net>
2020-01-30 17:46:29 -0500

    Update Test

--- src/app/pos/remark-dialog/remark-dialog.component.spec.ts
+++ src/app/pos/remark-dialog/remark-dialog.component.spec.ts
@@ -1,5 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from '../../material/material.module';
 import { RemarkDialogComponent } from './remark-dialog.component';
 
 describe('RemarkDialogComponent', () => {
@@ -8,9 +12,18 @@ describe('RemarkDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RemarkDialogComponent ]
+      imports: [
+        ReactiveFormsModule,
+        MaterialModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
+      declarations: [RemarkDialogComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
