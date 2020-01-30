108e462477ef2a16fc6d99fe6012154b32d2714c
Jan Moxter <jan.moxter@moxter.net>
2020-01-30 18:04:59 -0500

    Update Test

--- src/app/spots/extended-spot-picker/extended-spot-picker.component.spec.ts
+++ src/app/spots/extended-spot-picker/extended-spot-picker.component.spec.ts
@@ -1,6 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ImageService, PosConfigToken, PosService, SpotService, SpotsLayout } from '@efaps/pos-library';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockPipe } from 'ng-mocks';
+import { Observable } from 'rxjs';
 
+import { MaterialModule } from '../../material/material.module';
 import { ExtendedSpotPickerComponent } from './extended-spot-picker.component';
+
+class PosServiceStub { }
+class SpotServiceStub {
+  getLayout(): Observable<SpotsLayout> {
+    return new Observable()
+  }
+}
+class ImageServiceStub { }
 
 describe('ExtendedSpotPickerComponent', () => {
   let component: ExtendedSpotPickerComponent;
@@ -8,9 +23,23 @@ describe('ExtendedSpotPickerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExtendedSpotPickerComponent ]
+      imports: [
+        MaterialModule,
+        RouterTestingModule,
+        HttpClientModule,
+      ],
+      providers: [
+        { provide: PosService, useClass: PosServiceStub },
+        { provide: SpotService, useClass: SpotServiceStub },
+        { provide: ImageService, useClass: ImageServiceStub },
+        { provide: PosConfigToken, useValue: {} },
+      ],
+      declarations: [
+        ExtendedSpotPickerComponent,
+        MockPipe(TranslatePipe)
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
