65ad776a1f0518f68bca57c908d6fd2b88a08a7b
Jan Moxter <jan.moxter@moxter.net>
2020-01-30 17:43:24 -0500

    Update Test

--- src/app/pos/product-grid/product-grid.component.spec.ts
+++ src/app/pos/product-grid/product-grid.component.spec.ts
@@ -1,13 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MockComponent, MockDirective, MockPipe } from 'ng-mocks';
+import { PosConfigToken, PosCurrencyPipe, PosService, ProductService, SecurePipe } from '@efaps/pos-library';
+import { MockPipe } from 'ng-mocks';
 import { Observable } from 'rxjs/Observable';
 
 import { MaterialModule } from '../../material/material.module';
-import { PaymentService, PosService, ProductService, SecurePipe, WorkspaceService } from '@efaps/pos-library';
 import { ProductGridComponent } from './product-grid.component';
-
 
 class PosServiceStub {
   currentOrder = new Observable(observer => {
@@ -34,14 +34,17 @@ describe('ProductgridComponent', () => {
       imports: [
         BrowserAnimationsModule,
         MaterialModule,
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
       ],
       providers: [
+        { provide: PosConfigToken, useValue: {} },
         { provide: PosService, useClass: PosServiceStub },
         { provide: ProductService, useClass: ProductServiceStub }
       ],
       declarations: [
         ProductGridComponent,
+        MockPipe(PosCurrencyPipe),
         MockPipe(SecurePipe)
       ]
     })
