8877422c84022ec185719497754233e4074c5928
Jan Moxter <jan.moxter@moxter.net>
2020-03-28 00:50:02 -0500

    Fixing test

--- src/app/shared/taxpayer-query/taxpayer-query.component.spec.ts
+++ src/app/shared/taxpayer-query/taxpayer-query.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaxpayerQueryComponent } from './taxpayer-query.component';
+import { TaxpayerService } from '@efaps/pos-library';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../../material/material.module';
+import { RouterTestingModule } from '@angular/router/testing/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+class TaxpayerServiceStub {
+
+}
 
 describe('TaxpayerQueryComponent', () => {
   let component: TaxpayerQueryComponent;
@@ -8,7 +18,13 @@ describe('TaxpayerQueryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TaxpayerQueryComponent ]
+      imports: [
+        ReactiveFormsModule,
+      ],
+      declarations: [ TaxpayerQueryComponent ],
+      providers: [
+        { provide: TaxpayerService, useClass: TaxpayerServiceStub }
+      ],
     })
     .compileComponents();
   }));
