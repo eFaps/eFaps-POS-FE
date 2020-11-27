e743ed0e3cee222c5389eb9346334e0fb562ea04
Jan Moxter <jan@moxter.net>
2020-11-27 18:11:10 -0500

    Lazy loaded elements

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit } from "@angular/core";
-import { AdminService, Versions } from "@efaps/pos-library";
+import {
+  AdminService,
+  ConfigService,
+  Versions,
+  Extension
+} from "@efaps/pos-library";
 import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-admin",
   templateUrl: "./admin.component.html",
-  styleUrls: ["./admin.component.scss"],
+  styleUrls: ["./admin.component.scss"]
 })
 export class AdminComponent implements OnInit {
   busy: Subscription;
   versions: Versions;
-  pinpadsUrl = "extensions/pinpad-element.js"
-
-  constructor(private adminService: AdminService) {}
+  pinpadsUrl = "extensions/pinpad-element.js";
+  lazyElements: Extension[] = [];
+  constructor(
+    private adminService: AdminService,
+    private configService: ConfigService
+  ) {}
 
   ngOnInit() {
     this.adminService
       .version()
-      .subscribe((versions) => (this.versions = versions));
+      .subscribe(versions => (this.versions = versions));
+    this.configService.getExtensions().subscribe({
+      next: extensions => {
+        extensions
+          .filter(extension => extension.key === "admin")
+          .forEach(extension => {
+            this.lazyElements.push(extension);
+          });
+      }
+    });
   }
 
   reload() {
