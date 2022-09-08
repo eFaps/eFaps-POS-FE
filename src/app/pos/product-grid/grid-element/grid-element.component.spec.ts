a35425770542c4cc51830e5a89a2858b46c809d6
Jan Moxter <jan@moxter.net>
2022-09-07 19:04:58 -0500

    Update tests

--- src/app/pos/product-grid/grid-element/grid-element.component.spec.ts
+++ src/app/pos/product-grid/grid-element/grid-element.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatTabsModule } from "@angular/material/tabs";
 
 import { GridElementComponent } from "./grid-element.component";
 
@@ -8,6 +9,7 @@ describe("GridElementComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatTabsModule],
       declarations: [GridElementComponent],
     }).compileComponents();
 
--- src/app/pos/product-grid/products-element/products-element.component.spec.ts
+++ src/app/pos/product-grid/products-element/products-element.component.spec.ts
@@ -1,14 +1,38 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatDialogModule } from "@angular/material/dialog";
+import { PosConfigToken, PosService, ProductService } from "@efaps/pos-library";
+import { Observable } from "rxjs";
 
 import { ProductsElementComponent } from "./products-element.component";
-
+class PosServiceStub {
+  currentOrder = new Observable((observer) => {
+    observer.next({});
+  });
+  currentTicket = new Observable((observer) => {
+    observer.next({});
+  });
+}
+class ProductServiceStub {
+  getPosCategories() {
+    return new Observable((observer) => {
+      observer.next([]);
+    });
+  }
+}
 describe("ProductsElementComponent", () => {
   let component: ProductsElementComponent;
   let fixture: ComponentFixture<ProductsElementComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, MatDialogModule],
       declarations: [ProductsElementComponent],
+      providers: [
+        { provide: PosConfigToken, useValue: {} },
+        { provide: PosService, useClass: PosServiceStub },
+        { provide: ProductService, useClass: ProductServiceStub },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsElementComponent);
