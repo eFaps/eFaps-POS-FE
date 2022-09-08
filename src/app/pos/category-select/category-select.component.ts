a7a574a9839a0f8ad85f135389b50c35ef1ebeb7
Jan Moxter <jan@moxter.net>
2022-09-07 22:13:23 -0500

    Category select

--- src/app/pos/category-select/category-select.component.ts
+++ src/app/pos/category-select/category-select.component.ts
@@ -9,16 +9,19 @@ import { Category, ProductService } from "@efaps/pos-library";
 })
 export class CategorySelectComponent implements OnInit {
   categories: Category[] = [];
-
+  rootCategories: Category[] = [];
   constructor(
     private productService: ProductService,
     public dialogRef: MatDialogRef<CategorySelectComponent>
   ) {}
 
   ngOnInit() {
-    this.productService
-      .getCategories()
-      .subscribe((_categories) => (this.categories = _categories));
+    this.productService.getCategories().subscribe((_categories) => {
+      this.categories = _categories;
+      this.rootCategories = this.categories.filter(
+        (category) => category.parentOid == null
+      );
+    });
   }
 
   selectCategory(index: number) {
