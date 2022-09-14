09a215cf1be9a3e7d880f5aeec5d3c9baeb9c785
Jan Moxter <jan@moxter.net>
2022-09-14 12:57:22 -0500

    Update test

--- src/app/balance/balance-summary/balance-summary.component.spec.ts
+++ src/app/balance/balance-summary/balance-summary.component.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { ComponentFixture, TestBed, async } from "@angular/core/testing";
 import { MatDialogModule } from "@angular/material/dialog";
 import { PosConfigToken } from "@efaps/pos-library";
-import { MockComponent } from "ng-mocks";
+import { Mock, MockComponent } from "ng-mocks";
 
 import { BalanceSummarySectionComponent } from "../balance-summary-section/balance-summary-section.component";
 import { BalanceSummaryComponent } from "./balance-summary.component";
@@ -25,6 +25,28 @@ describe("BalanceSummaryComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BalanceSummaryComponent);
     component = fixture.componentInstance;
+    const detail = {
+      documentCount: 0,
+      paymentCount: 0,
+      netTotal: 0,
+      crossTotal: 0,
+      paymentInfos: [],
+      taxEntries: []
+    }
+    component.summary = {
+      balance: {
+        id: "",
+        oid: "",
+        number: "",
+        startAt: new Date(),
+        endAt: new Date(),
+        status: "OPEN"
+      },
+      detail: detail,
+      invoiceDetail: detail,
+      receiptDetail: detail,
+      ticketDetail: detail
+    }
     fixture.detectChanges();
   });
 
