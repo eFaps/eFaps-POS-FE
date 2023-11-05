38f4bc5f44b23c4d5e25df5246fc798259dc1f61
Jan Moxter <jan@moxter.net>
2023-11-04 20:36:19 -0500

    Update ErrorInterceptor

--- src/app/services/error.interceptor.ts
+++ src/app/services/error.interceptor.ts
@@ -7,29 +7,30 @@ import {
 } from "@angular/common/http";
 
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
 import { Router } from "@angular/router";
-import { tap } from "rxjs/operators";
+import { catchError } from "rxjs/operators";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private snackBar: MatSnackBar) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
-      tap(
-        (event: HttpEvent<any>) => {},
-        (err: any) => {
-          if (err instanceof HttpErrorResponse) {
-            if (err.status === 401) {
-              this.router.navigate(["/login"]);
-            }
-          }
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.router.navigate(["/login"]);
+        } else {
+          this.snackBar.open(error.statusText, "close", {
+            duration: 2000
+          });
         }
-      )
+        return throwError(() => new Error(error.message));
+      })
     );
   }
 }
