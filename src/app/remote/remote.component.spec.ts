743a2614db884b2b93f854bcbe0e91a38d9fca0e
Jan Moxter <jan@moxter.net>
2025-04-29 20:17:12 -0500

    Correct test

--- src/app/remote/remote.component.spec.ts
+++ src/app/remote/remote.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { RemoteComponent } from "./remote.component";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { PosConfigToken } from "@efaps/pos-library";
 
 describe("RemoteComponent", () => {
   let component: RemoteComponent;
@@ -8,6 +11,11 @@ describe("RemoteComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+         { provide: PosConfigToken, useValue: {} },
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting()
+      ],
       imports: [RemoteComponent],
     }).compileComponents();
 
